4358bb3f0649624be2bdcf2b69e5e41cec88d9bc
mutjin <104980932+mutjin@users.noreply.github.com>
2022-06-28 08:53:38 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import {StatusBar, SafeAreaView,Text } from 'react-native';
+import {viewStyles,textStyles} from './styles';
+import Input from './components/Input';
+import {images} from './images';
+import IconButton from './components/IconButton';
+
+export default function App(){
+
+    /*aleart events*/
+    const [newTask,setNewTask]=useState('');
+
+    const _addTask=()=>{
+        alert(`Add: ${newTask}`);
+        setNewTask('');
+    };
+
+    const _handleTextChange = text => {
+        setNewTask(text);
+    };
+    /*aleart events*/
+
+
+    /*set title and background*/
+    return(
+        <SafeAreaView style={viewStyles.container}>
+            <StatusBar barStyle="light-content" style={textStyles.StatusBar}/>
+            <Text style={textStyles.title}>Walk Goyang</Text>
+            <Input value={newTask} onChangeText={_handleTextChange} onSubmitEditing={_addTask}/>
+            <IconButton type={images.uncompleted}/>
+            <IconButton type={images.completed}/>
+            <IconButton type={images.delete}/>
+            <IconButton type={images.update}/>
+        </SafeAreaView>
+    );
+    /*set title and background*/
+};
